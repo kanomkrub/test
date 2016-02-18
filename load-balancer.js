23c09536d34c0be48cd0b0099e5adcffbaa6a4b1
kanomkrub <iners.kool@gmail.com>
2016-02-18 16:22:03 +0700

    log request

--- load-balancer.js
+++ load-balancer.js
@@ -1,32 +1,43 @@
+//vee reverse proxy for loadbalance imagin kub
 var args = process.argv.splice(2);
 var httpProxy = require('http-proxy');
 var http = require('http');
 
-var addresses = [
-    {
-		target: 'http://localhost:8001'
-    },
-    {
-		target: 'http://localhost:8002'
-    },
-    {
-		target: 'http://localhost:8003'
-    }
-];
 
+var round_robin_listenPort = 5050;
+var sticky_iphash_listenPort = 5051;
+var addresses = [
+	{target:'http://172.20.36.201:80'}
+    // {
+		// target: 'http://172.20.36.201:8001'
+    // },
+    // {
+		// target: 'http://172.20.36.201:8002'
+    // },
+    // {
+		// target: 'http://172.20.36.201:8003'
+    // }
+];
+console.log
+('-------------------------------------------------------');
 console.log('-available direct targets');
 addresses.forEach(function (target) {
-	console.log(JSON.stringify(target));
+	console.log("--"+JSON.stringify(target));
 });
+console.log
+('-------------------------------------------------------');
 
 var i = 0;
 var rrProxy = httpProxy.createProxyServer({});
 var rrServer = http.createServer(function (req, res) {
+	console.log(req.url+' redirected to '+JSON.stringify
+(addresses[i]));
 	rrProxy.web(req, res, addresses[i]);
 	i = (i + 1) % addresses.length;
 });
-rrServer.listen(5050);
-console.log("-start round robin loadbalancer on port 5050")
+rrServer.listen(round_robin_listenPort);
+console.log("-start round robin loadbalancer on port 
+"+round_robin_listenPort);
 
 
 var iphProxy = httpProxy.createProxyServer({});
@@ -45,7 +56,10 @@ var iphserver = http.createServer(function (req, res) {
 		hash = hash & hash;
 	}
 	var server = hash % addresses.length;
+	console.log(req.url+' redirected to '+JSON.stringify
+(addresses[server]));
 	iphProxy.web(req, res, addresses[server]);
 });
-iphserver.listen(5051);
-console.log("-start sticky(ip-hash) loadbalancer on port 5051")
+iphserver.listen(sticky_iphash_listenPort);
+console.log("-start sticky(ip-hash) loadbalancer on port 
+"+sticky_iphash_listenPort);
