309a5cdc463c3208a0892e1ca62399180cd43680
kanomkrub <iners.kool@gmail.com>
2016-03-09 15:44:14 +0700

    fix ip-hash bug

--- load-balancer.js
+++ load-balancer.js
@@ -4,10 +4,12 @@ var httpProxy = require('http-proxy');
 var http = require('http');
 
 
-var round_robin_listenPort = 5050;
-var sticky_iphash_listenPort = 5051;
+var round_robin_listenPort = 2000;
+var sticky_iphash_listenPort = 2001;
 var addresses = [
-	{target:'http://172.20.36.201:80'}
+	{target:'http://172.20.36.201:3001'},
+	{target:'http://172.20.36.201:3002'}
+	
     // {
 		// target: 'http://172.20.36.201:8001'
     // },
@@ -18,26 +20,22 @@ var addresses = [
 		// target: 'http://172.20.36.201:8003'
     // }
 ];
-console.log
-('-------------------------------------------------------');
+console.log('-------------------------------------------------------');
 console.log('-available direct targets');
 addresses.forEach(function (target) {
 	console.log("--"+JSON.stringify(target));
 });
-console.log
-('-------------------------------------------------------');
+console.log('-------------------------------------------------------');
 
 var i = 0;
 var rrProxy = httpProxy.createProxyServer({});
 var rrServer = http.createServer(function (req, res) {
-	console.log(req.url+' redirected to '+JSON.stringify
-(addresses[i]));
+	console.log(req.url+' redirected to '+JSON.stringify(addresses[i]));
 	rrProxy.web(req, res, addresses[i]);
 	i = (i + 1) % addresses.length;
 });
 rrServer.listen(round_robin_listenPort);
-console.log("-start round robin loadbalancer on port 
-"+round_robin_listenPort);
+console.log("-start round robin loadbalancer on port "+round_robin_listenPort);
 
 
 var iphProxy = httpProxy.createProxyServer({});
@@ -55,11 +53,9 @@ var iphserver = http.createServer(function (req, res) {
 		hash = ((hash << 5) - hash) + char;
 		hash = hash & hash;
 	}
-	var server = hash % addresses.length;
-	console.log(req.url+' redirected to '+JSON.stringify
-(addresses[server]));
+	var server = Math.abs(hash % addresses.length)-1;
+	console.log(req.url+' redirected to '+JSON.stringify(addresses[server]));
 	iphProxy.web(req, res, addresses[server]);
 });
 iphserver.listen(sticky_iphash_listenPort);
-console.log("-start sticky(ip-hash) loadbalancer on port 
-"+sticky_iphash_listenPort);
+console.log("-start sticky(ip-hash) loadbalancer on port "+sticky_iphash_listenPort);
